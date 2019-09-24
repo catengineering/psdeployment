21092b354eb1960b21f9597792d3227670e51c63
johm-msft <johm@microsoft.com>
2019-09-23 20:14:08 -0700

    scripts and json

---
+++ PSDeploy.ps1
@@ -0,0 +1,53 @@
+﻿
+function DeployTemplate([string]$pathtotemplate,[string]$pathtoparameters,[string]$location)
+{
+ $resourcegroupname = "testerrg" + ([Datetime]::Now).ToShortDateString().Replace("/","-")
+ New-AzResourceGroup -Name $resourcegroupname -Location $location 
+ $name = 'AzureTest' + $location + ([Datetime]::Now).ToShortDateString().Replace("/","-")
+ $deploymentinfo = New-AzResourceGroupDeployment -Name $name -ResourceGroupName $resourcegroupname -TemplateParameterFile $pathtoparameters -TemplateFile $pathtotemplate 
+ return $deploymentinfo
+}
+function GetRegion
+{
+    $allregions = Get-AzLocation |select Location
+    $max = $allregions.location.count
+    $randomindex = Get-Random -Maximum $max -Minimum 0
+    return $allregions[$randomindex].Location
+}
+
+function GetDeploymentInfo([string]$deploymentname,[string]$resourcegroup,[string]$location)
+{
+  $results = Get-AzResourceGroupDeploymentOperation -ResourceGroupName $resourcegroup -DeploymentName $deploymentname
+  $resultsArray = @()
+  foreach($result in $results)
+  {
+     $obj = New-Object psobject    
+     $resource = $result.properties.targetResource.id
+     $deployResult = $result.properties.provisioningState
+     $deployDuration = $result.properties.duration 
+
+
+     $obj | Add-Member -MemberType NoteProperty -Name ResourceID -Value $resource
+     $obj | Add-Member -MemberType NoteProperty -Name deploymentName -Value $deploymentname
+     $obj | Add-Member -MemberType NoteProperty -Name Location -value $location
+     $obj | Add-Member -MemberType NoteProperty -Name deployResult -Value $deployResult
+     $obj | Add-Member -MemberType NoteProperty -Name deployDuration -Value $deployDuration
+     $resultsArray += $obj 
+  }
+  return $resultsArray
+}
+
+### Paths for Templates
+$templateName = "azuredeploy.json"
+$templateParameters = "azuredeploy.parameters.json"
+$rootpath = "C:\Users\johm\source\projectannandale\ARM Templates for Automated Deployment\TemplateTester\"
+$vnetvmss = $rootpath + "templateb-vnet-vmss\templateb-vnet-vmss\"
+$vnetvmsstemplate = $vnetvmss + $templateName
+$vnetvmsstemplateparams = $vnetvmss + $templateParameters
+###
+
+$location = GetRegion
+
+$info = DeployTemplate $vnetvmsstemplate $vnetvmsstemplateparams $location
+
+$output = GetDeploymentInfo $info[1].DeploymentName $info[1].ResourceGroupName $info[0].Location
